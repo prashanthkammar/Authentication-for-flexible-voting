6bbbbf7554f1444f807cf7ee19731ed836f545f6
Prashanth G K <prashkammar17@gmail.com>
2020-06-08 09:55:14 +0530

    small change

--- flexible_voter.m
+++ flexible_voter.m
@@ -4,23 +4,25 @@ conn = database('MySQL','root','');
 
 disp("Authentication for Flexible voting ");
 voter_id = input("Enter You voter ID: ", 's');
-
+        
 loc = voter_id(1:3);
 if loc == "ben"
-    evm = bengalurufig;
-    sqlquery = ['select * from voter.bengaluru where voterid = ''',voter_id,''''];
+    sqlquery = ['select * from bengaluru where voterid = ''',voter_id,''''];
 elseif loc == "mys"
-    evm = mysuru;
-    sqlquery = ['select * from voter.mysuru where voterid = ''',voter_id,''''];
+    sqlquery = ['select * from mysuru where voterid = ''',voter_id,''''];
 end
 
 result = fetch(conn,sqlquery);
-
+clc;
+greet = "Hello " + result.name;
+disp(greet);
 status = result.status;
 if status == 1
     disp("You have voted already");
-else
-    evm;        
+elseif loc == "ben"
+    bengalurufig;
+elseif loc == "mys"
+    mysuru;
 end
 
 close(conn);
